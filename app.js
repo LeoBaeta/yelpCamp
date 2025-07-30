26f44a3d79e1a34698cb4157e5a16b32c50b8efb
Leonardo Baeta Lopes <leonardobaeta@gmail.com>
2025-07-30 13:00:33 -0300

    Added basic styles to the web site
    - Added ejs-mate package for working with templates
    - Added basic styling with Bootstrap 5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
 const Campground = require('./models/Campground');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -18,6 +19,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
